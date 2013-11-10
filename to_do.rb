3381b6dec6e72ab9e59a3ef5abca6fecbe0d95a9
Brian Carroll <briancarroll@Adminstrators-MacBook-Pro.local>
2013-11-10 16:26:44 -0500

    Updated with better UI

--- to_do.rb
+++ to_do.rb
@@ -1,5 +1,5 @@
 to_do = Hash.new
-puts "This is your To Do list!"
+puts 'This is your {To Do} list!'
 
 def ask
 		puts "\n"
@@ -14,41 +14,52 @@ end
 
 ask
 input = gets.chomp
-
 list_num = 1
 
 while input != "quit" do
-
 	case input
 		when "add"
 			puts "Add something you need to do"
 			new_item = gets.chomp
-			to_do[list_num.to_s.to_sym] = new_item.to_s
+			to_do[list_num.to_i] = new_item.to_s
 			list_num += 1
 			puts "Added #{new_item} to your To Do list!"
+			puts "----------------"
+			puts "Your To Do list:"
+			puts "\n"
 			puts to_do.inspect
+			puts "----------------"
 		when "remove"
 			puts "View the list below and choose a number to remove"
+			puts "----------------"
+			puts "Your To Do list:"
 			puts "\n"
-			puts "Your To Do list: " + to_do.inspect
+			puts to_do.inspect
+			puts "----------------"
 			removed_item = gets.chomp
-			if to_do[removed_item.to_sym]
-				to_do.delete(removed_item.to_sym)
+			if to_do[removed_item.to_i]
+				to_do.delete(removed_item.to_i)
 				list_num -= 1
 			else
 				puts "Not on the list!"
 			end 
 			puts "Removed #{removed_item}"
-			puts to_do.inspect
-		when "clear"
-			to_do = {}
-			puts "Cleared the list!"
-			puts to_do.inspect
-		when "show"
+			puts "----------------"
 			puts "Your To Do list:"
 			puts "\n"
 			puts to_do.inspect
-
+			puts "----------------"
+		when "clear"
+			to_do.clear
+			puts "Cleared the list!"
+			list_num = 1
+			puts to_do.inspect
+		when "show"
+			puts "----------------"
+			puts "Your To Do list:"
+			puts "\n"
+			puts to_do.inspect
+			puts "----------------"
 	end
 
 	ask
