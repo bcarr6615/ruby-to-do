6dd86341ddbcda908b23911d6322fabf84f8f79a
Brian Carroll <briancarroll@Adminstrators-MacBook-Pro.local>
2013-11-10 18:35:15 -0500

    Better text UI

--- to_do.rb
+++ to_do.rb
@@ -1,25 +1,31 @@
 to_do = Hash.new
+puts "\n"
 puts 'This is your {To Do} list!'
 
 def ask
-		puts "\n"
-		puts "Please type one of the following commands..."
-		puts "\n"
-		puts "-----'add' to add a new item to your list"
-		puts "-----'remove' to remove an item from your list"
-		puts "-----'clear' to clear out the entire list"
-		puts "-----'show' to show your list"
-		puts "-----'quit' to quit"
+	puts "\n"
+	puts "Please type one of the following commands..."
+	puts "\n"
+	puts "add -- remove -- clear -- show -- quit"
 end
 
-ask
+puts "\n"
+puts "Please type one of the following commands..."
+puts "\n"
+puts "-----'add' to add a new item to your list"
+puts "-----'remove' to remove an item from your list"
+puts "-----'clear' to clear out the entire list"
+puts "-----'show' to show your list"
+puts "-----'quit' to quit"
+
 input = gets.chomp
 list_num = 1
 
 while input != "quit" do
 	case input
 		when "add"
-			puts "Add something you need to do"
+			puts "\n"
+			puts "Add something you need to do:"
 			new_item = gets.chomp
 			to_do[list_num.to_i] = new_item.to_s
 			list_num += 1
@@ -30,7 +36,8 @@ while input != "quit" do
 			puts to_do.inspect
 			puts "----------------"
 		when "remove"
-			puts "View the list below and choose a number to remove"
+			puts "\n"
+			puts "View the list below and choose a number to remove:"
 			puts "----------------"
 			puts "Your To Do list:"
 			puts "\n"
@@ -38,23 +45,29 @@ while input != "quit" do
 			puts "----------------"
 			removed_item = gets.chomp
 			if to_do[removed_item.to_i]
+				removed_value = to_do[removed_item.to_i]
 				to_do.delete(removed_item.to_i)
 				list_num -= 1
+				puts "\n"
+				puts "Removed #{removed_value}"
+				puts "\n"
+				puts "----------------"
+				puts "Your To Do list:"
+				puts "\n"
+				puts to_do.inspect
+				puts "----------------"
 			else
 				puts "Not on the list!"
 			end 
-			puts "Removed #{removed_item}"
-			puts "----------------"
-			puts "Your To Do list:"
-			puts "\n"
-			puts to_do.inspect
-			puts "----------------"
+		
 		when "clear"
+			puts "\n"
 			to_do.clear
 			puts "Cleared the list!"
 			list_num = 1
 			puts to_do.inspect
 		when "show"
+			puts "\n"
 			puts "----------------"
 			puts "Your To Do list:"
 			puts "\n"
@@ -65,7 +78,7 @@ while input != "quit" do
 	ask
 	input = gets.chomp
 end
-
+puts "\n"
 puts "Goodbye!"
 puts "\n"
 
